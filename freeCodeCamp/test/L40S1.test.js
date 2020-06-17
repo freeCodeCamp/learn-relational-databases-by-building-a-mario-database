bfa8ae15808808262aa1b4f73255b2f9aa11b8fe
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 13:27:28 -0500

    40.1

---
+++ freeCodeCamp/test/L40S1.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { client } = require('./utils');
+
+describe('You', () => {
+  it('should have a database named "first_database"', async () => {
+    const query = "SELECT datname FROM pg_catalog.pg_database WHERE datname='first_database';";
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+      assert(res.rowCount && res.rowCount === 1);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
