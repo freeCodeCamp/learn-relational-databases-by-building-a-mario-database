1ebe4a7d3024b17afc39b7fdd711b5ba4230c0b2
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 14:26:13 -0500

    50.1

--- .freeCodeCamp/db.sql
+++ .freeCodeCamp/db.sql
@@ -1,7 +1,49 @@
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE usename = 'freecodecamp';
+
 --
 -- PostgreSQL database dump
 --
 
 -- Dumped from database version 12.3 (Debian 12.3-1.pgdg90+1)
 -- Dumped by pg_dump version 12.3 (Debian 12.3-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE IF EXISTS first_database;
+--
+-- Name: first_database; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE first_database WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE first_database OWNER TO freecodecamp;
+
+-- \connect first_database
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
 
