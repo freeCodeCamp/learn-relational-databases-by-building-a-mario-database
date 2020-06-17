4074f0aae0714ce0021a386d272aa7f5d2003d3b
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 14:26:13 -0500

    50.1

--- .freeCodeCamp/test/40.test.js
+++ .freeCodeCamp/test/40.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'postgres';
@@ -21,4 +21,4 @@ describe('You', () => {
       await client.end();
     }
   });
-});
+});*/
