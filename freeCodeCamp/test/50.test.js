c664b0634f63fe3a2ca12eb01754a94634f7199c
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 14:39:16 -0500

    60.1

--- freeCodeCamp/test/50.test.js
+++ freeCodeCamp/test/50.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
     const l = /statement:SELECTd\.datnameas"Name",pg_catalog\.pg_get_userbyid\(d\.datdba\)as"Owner",pg_catalog\.pg_encoding_to_char\(d\.encoding\)as"Encoding",d\.datcollateas"Collate",d\.datctypeas"Ctype",pg_catalog\.array_to_string\(d\.datacl,E'\\n'\)AS"Accessprivileges"FROMpg_catalog\.pg_databasedORDERBY1;/;
     assert(l.test(lastLog));
   });
-});
+});*/
