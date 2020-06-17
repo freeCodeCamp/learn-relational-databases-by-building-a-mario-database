6a8f072071a10cff6918bc84a722442d1717abcb
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 12:05:32 -0500

    20.1

---
+++ freeCodeCamp/test/L20S1.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should be logged in to postgresql', () => {
+    const re = /connectionauthorized:user=freecodecampdatabase=postgresapplication_name=psql/;
+    assert(re.test(lastLog));
+  });
+});
