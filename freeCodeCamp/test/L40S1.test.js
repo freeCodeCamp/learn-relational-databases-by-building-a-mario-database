ec86cba4876006a1c44d2995cc42898558c4b77a
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 14:26:13 -0500

    50.1

--- freeCodeCamp/test/L40S1.test.js
+++ freeCodeCamp/test/L40S1.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { client } = require('./utils');
 
 describe('postgresql', () => {
@@ -14,6 +14,6 @@ describe('postgresql', () => {
       await client.end();
     }
   });
-});
+});*/
 
 // solution
