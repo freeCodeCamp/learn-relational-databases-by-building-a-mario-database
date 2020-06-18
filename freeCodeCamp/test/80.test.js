dd0c8af3e5a389fb1cbcbfa851b44572f8bd8cc7
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 21:31:34 -0500

    80.1

---
+++ freeCodeCamp/test/80.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should be connected to "second_database"', () => {
+    const re = /connectionauthorized:user=freecodecampdatabase=second_databaseapplication_name=psql/;
+    assert(re.test(lastLog));
+  });
+});
