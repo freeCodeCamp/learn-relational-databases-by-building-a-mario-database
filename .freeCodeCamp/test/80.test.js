bdc68dba75253499d6d5a64bb4e8594e5a143cd4
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-17 21:36:31 -0500

    90.1

--- .freeCodeCamp/test/80.test.js
+++ .freeCodeCamp/test/80.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -8,4 +8,4 @@ describe('You', () => {
 
     assert(re.test(lastLog));
   });
-});
+});*/
