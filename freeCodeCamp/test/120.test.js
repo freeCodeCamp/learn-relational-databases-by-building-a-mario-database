0c72ad1156ec4122bce05a539a93170bdddc04bf
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-29 12:43:31 -0500

    120.1

---
+++ freeCodeCamp/test/120.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'second_database';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('"second_database"', () => {
+  it('should have a table named "second_table"', async () => {
+    const query = 'SELECT * FROM second_table;'
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+      assert(!!res);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
