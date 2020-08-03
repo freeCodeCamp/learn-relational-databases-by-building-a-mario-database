54db5fd82635ae5288ba9e81b8ad968f08d38943
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-03 16:36:36 -0500

    340.1

--- freeCodeCamp/test/330.test.js
+++ freeCodeCamp/test/330.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
     const selectAll = /statement:select\*fromsecond_table;/;
     assert(selectAll.test(lastLog));
   });
-});
+});*/
