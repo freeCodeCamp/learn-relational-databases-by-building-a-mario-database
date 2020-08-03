e797dc1fa8b8d8c7811e04b2ff8a53429621880d
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-03 16:50:54 -0500

    400.1

--- freeCodeCamp/test/390.test.js
+++ freeCodeCamp/test/390.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
     const dSecondTable = /statement:SELECTc\.oid,n\.nspname,c\.relnameFROMpg_catalog\.pg_classcLEFTJOINpg_catalog\.pg_namespacenONn\.oid=c\.relnamespaceWHEREc\.relnameOPERATOR\(pg_catalog\.~\)'\^\(second_table\)\$'COLLATEpg_catalog\.defaultANDpg_catalog\.pg_table_is_visible\(c\.oid\)ORDERBY2,3;/;
     assert(dSecondTable.test(lastLog));
   });
-});
+});*/
