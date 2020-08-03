abb82e69f67e7b61024cab80dd904bb6d2c9b49d
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-03 16:38:36 -0500

    350.1

---
+++ freeCodeCamp/test/350.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should use "SELECT" to view all the data in "second_table"', () => {
+    const selectAll = /statement:select\*fromsecond_table;/;
+    assert(selectAll.test(lastLog));
+  });
+});
