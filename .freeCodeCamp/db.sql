90326b610d9bfdaa3996965f76d4076ab5ad19d9
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-03 16:38:36 -0500

    350.1

--- .freeCodeCamp/db.sql
+++ .freeCodeCamp/db.sql
@@ -81,7 +81,6 @@ ALTER TABLE public.second_table OWNER TO freecodecamp;
 
 INSERT INTO public.second_table VALUES (1, 'Samus');
 INSERT INTO public.second_table VALUES (2, 'Mario');
-INSERT INTO public.second_table VALUES (3, 'Luigi');
 
 
 --
