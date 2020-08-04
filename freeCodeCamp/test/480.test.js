449ca9a9b605c2e39310d82bf67e2c282af61e24
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 12:13:48 -0500

    480.1

---
+++ freeCodeCamp/test/480.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should be connected to "mario_database"', () => {
+    const re = /connectionauthorized:user=freecodecampdatabase=mario_databaseapplication_name=psql/;
+    assert(re.test(lastLog));
+  });
+});
