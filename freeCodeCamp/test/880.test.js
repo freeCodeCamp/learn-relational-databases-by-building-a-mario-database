c336156acc87a8324382be8c629ecb3c6301e1a7
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 18:14:39 -0500

    880.1

---
+++ freeCodeCamp/test/880.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true, 8);
+  });
+
+  it('should display the details of the "characters" table', () => {
+    const dCharactersTable = /statement:SELECTc\.oid,n\.nspname,c\.relnameFROMpg_catalog\.pg_classcLEFTJOINpg_catalog\.pg_namespacenONn\.oid=c\.relnamespaceWHEREc\.relnameOPERATOR\(pg_catalog\.~\)'\^\(characters\)\$'COLLATEpg_catalog\.defaultANDpg_catalog\.pg_table_is_visible\(c\.oid\)ORDERBY2,3;/;
+    assert(dCharactersTable.test(lastLog));
+  });
+});
