b729ff61f59a815b07c212b562e4825813c249ed
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 16:03:41 -0500

    720.1

--- .freeCodeCamp/test/710.test.js
+++ .freeCodeCamp/test/710.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'mario_database';
@@ -34,4 +34,4 @@ describe('You', () => {
       await client.end();
     }
   });
-});
+});*/
