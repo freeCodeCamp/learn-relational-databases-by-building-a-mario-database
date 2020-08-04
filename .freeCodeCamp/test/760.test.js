8aa6f405fd625b4e3211a05ccb602de208f95df2
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 16:11:32 -0500

    760.1

--- .freeCodeCamp/test/760.test.js
+++ .freeCodeCamp/test/760.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'mario_database';
@@ -15,14 +15,14 @@ describe('Bowser', () => {
       await client.connect();
       const res = await client.query(query);
 
-      const notToadRow = res.rows.findIndex(row => {
-        return row.name === 'Toad' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Red' && row.character_id;
+      const notBowserRow = res.rows.findIndex(row => {
+        return row.name === 'Bowser' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Green' && row.character_id;
       });
 
       const correctRows = res.rows.filter(row => {
         const row1 = row.name === 'Luigi' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Green';
         const row2 = row.name === 'Peach' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Pink';
-        const row3 = row.name === 'Bowser' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Green';
+        const row3 = row.name === 'Bowser' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Yellow';
         const row4 = row.name === 'Yoshi' && row.homeland === 'Dinosaur Land' && row.favorite_color === 'Green';
         const row5 = row.name === 'Daisy' && row.homeland === 'Sarasaland' && row.favorite_color === 'Orange';
         const row6 = row.name === 'Toad' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Blue';
@@ -33,11 +33,11 @@ describe('Bowser', () => {
         }
       });
 
-      assert(notToadRow < 0 && correctRows.length >= 7);
+      assert(notBowserRow < 0 && correctRows.length >= 7);
     } catch (err) {
       assert(false);
     } finally {
       await client.end();
     }
   });
-});*/
+});
