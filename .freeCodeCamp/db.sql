ebdc6d62809520fd6e2f6be10adfda2bbb038687
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 12:58:51 -0500

    580.1

--- .freeCodeCamp/db.sql
+++ .freeCodeCamp/db.sql
@@ -52,7 +52,8 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.characters (
-    character_id integer NOT NULL
+    character_id integer NOT NULL,
+    name character varying(30) NOT NULL
 );
 
 
