6eeda3401fc330928169f067cc04dbd889e79ed7
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 16:11:32 -0500

    760.1

--- freeCodeCamp/test/760.test.js
+++ freeCodeCamp/test/760.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'mario_database';
@@ -7,7 +7,7 @@ const client = new Client({
   connectionString: connectionString,
 });
 
-describe('Toad', () => {
+describe('Bowser', () => {
   it('should have the correct "favorite_color"', async () => {
     const query = `SELECT * FROM characters;`;
 
@@ -15,15 +15,15 @@ describe('Toad', () => {
       await client.connect();
       const res = await client.query(query);
 
-      const toadRow = res.rows.findIndex(row => {
-        return row.name === 'Toad' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Blue' && row.character_id;
+      const bowserRow = res.rows.findIndex(row => {
+        return row.name === 'Bowser' && row.homeland === 'Mushroom Kingdom' && row.favorite_color === 'Yellow' && row.character_id;
       });
 
-      assert(toadRow >= 0);
+      assert(bowserRow >= 0);
     } catch (err) {
       assert(false);
     } finally {
       await client.end();
     }
   });
-});*/
+});
