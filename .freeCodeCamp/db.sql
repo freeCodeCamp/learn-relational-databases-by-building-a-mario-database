dd1d3e7781989cfae6d97792d0ff129bb65575bd
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-05 12:04:34 -0500

    1190.1

--- .freeCodeCamp/db.sql
+++ .freeCodeCamp/db.sql
@@ -157,6 +157,7 @@ INSERT INTO public.more_info VALUES (3, '1985-10-18', 173, 52.2, 3);
 INSERT INTO public.more_info VALUES (4, '1950-01-10', 66, 35.6, 4);
 INSERT INTO public.more_info VALUES (5, '1990-10-29', 258, 300.0, 5);
 INSERT INTO public.more_info VALUES (6, '1989-07-31', NULL, NULL, 6);
+INSERT INTO public.more_info VALUES (7, '1990-04-13', 162, 59.1, 7);
 
 
 --
@@ -170,7 +171,7 @@ SELECT pg_catalog.setval('public.characters_character_id_seq', 7, true);
 -- Name: more_info_more_info_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 6, true);
+SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 7, true);
 
 
 --
