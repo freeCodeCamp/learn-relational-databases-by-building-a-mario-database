f5f53f24d45a893138beef887f1713f0ee79d1c7
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-05 12:00:09 -0500

    1170.1

---
+++ freeCodeCamp/test/1170.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { getLastQueryResult } = require('./utils');
+
+describe('You', () => {
+  let lastQueryResult;
+  before(async () => {
+		lastQueryResult = await getLastQueryResult();
+  });
+
+  it('should view the "character_id" column from "characters" for only Yoshi\'s row', () => {
+		const correctCharacterId = /\|7\|/.test(lastQueryResult);
+		const correctNumberOfRows = /\(1row\)/.test(lastQueryResult);
+
+    assert(correctCharacterId && correctNumberOfRows);
+  });
+});
