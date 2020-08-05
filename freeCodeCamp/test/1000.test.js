d31560544cd7ca709209829ba367979a0a6f0f89
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 22:56:06 -0500

    1000.1

---
+++ freeCodeCamp/test/1000.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should view "character_id" column for all rows from "characters"', () => {
+    const selectAll = /statement:selectcharacter_idfromcharacters;/i;
+    assert(selectAll.test(lastLog));
+  });
+});
