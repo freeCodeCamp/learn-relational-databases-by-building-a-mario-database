fc680a395604442f459e7044d4be3fef3b799817
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-05 12:03:35 -0500

    1180.1

---
+++ freeCodeCamp/test/1180.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'mario_database';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('The "more_info" table', () => {
+  it('should have the correct row for "Bowser"', async () => {
+    const query = `SELECT * FROM more_info WHERE birthday = '1990-04-13' AND height = 162 AND weight = 59.1 AND character_id = 7;`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+      assert(res.rows.length >= 1);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
