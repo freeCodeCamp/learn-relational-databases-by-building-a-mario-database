3a90119ee35c0a6bd644854b4eff4ba009bd10c7
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-05 10:41:09 -0500

    1100.1

--- freeCodeCamp/test/1090.test.js
+++ freeCodeCamp/test/1090.test.js
@@ -1,20 +1,20 @@
-const assert = require('assert');
-const { getLastLog } = require('./utils');
+/*const assert = require('assert');
+const { getLastLog, getLastQueryResult } = require('./utils');
 
 describe('You', () => {
-  let lastLog;
+  let lastLog, lastQueryResult;
   before(async () => {
     lastLog = await getLastLog(true);
+		lastQueryResult = await getLastQueryResult();
   });
 
   it('should view the "character_id" and "name" column\'s from "more_info" for Toad\'s row', () => {
-    const select1 = /statement:selectname,character_idfromcharacterswherename='Toad';/i;
-    const select2 = /statement:selectcharacter_id,namefromcharacterswherename='Toad';/i;
-		const caseSensitive = /'Toad'/;
+    const select1 = /statement:selectname,character_idfromcharacterswherename='toad';/i;
+    const select2 = /statement:selectcharacter_id,namefromcharacterswherename='toad';/i;
 
-		const test1 = select1.test(lastLog) && caseSensitive.test(lastLog);
-		const test2 = select2.test(lastLog) && caseSensitive.test(lastLog);
+    const queryResultTest = /Toad/.test(lastQueryResult);
+    const selectTest = select1.test(lastLog) || select2.test(lastLog);
 
     assert(select1.test(lastLog) || select2.test(lastLog));
   });
-});
+});*/
