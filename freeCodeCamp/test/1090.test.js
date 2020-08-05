63ef2cc56939eaa379c9c4987d39b33767050025
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-05 10:39:13 -0500

    1090.1

---
+++ freeCodeCamp/test/1090.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const { getLastLog, getLastQueryResult } = require('./utils');
+
+describe('You', () => {
+  let lastLog, lastQueryResult;
+  before(async () => {
+    lastLog = await getLastLog(true);
+    lastQueryResult = await getLastQueryResult()
+  });
+
+  it('should view the "character_id" and "name" column\'s from "more_info" for Toad\'s row', () => {
+    const select1 = /statement:selectname,character_idfromcharacterswhere/i;
+    const select2 = /statement:selectcharacter_id,namefromcharacterswhere/i;
+    const correctColumnsInQuery = select1.test(lastLog) || select2.test(lastLog);
+    const correctNumberOfRows = /\(1row\)/.test(lastQueryResult);
+
+    assert(correctColumnsInQuery && correctNumberOfRows);
+  });
+});
