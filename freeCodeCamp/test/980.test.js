f3102635a5dd9ccf29cc4daf2a4778bf78c11590
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-04 22:52:29 -0500

    990.1

--- freeCodeCamp/test/980.test.js
+++ freeCodeCamp/test/980.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'mario_database';
@@ -22,4 +22,4 @@ describe('The "character_id" column', () => {
       await client.end();
     }
   });
-});
+});*/
