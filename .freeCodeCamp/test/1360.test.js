eb1312971d1b38f97f6981256c99a18bb4307dd2
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 12:03:41 -0500

    1370.1

--- .freeCodeCamp/test/1360.test.js
+++ .freeCodeCamp/test/1360.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { Client } = require('pg');
 
 const database = 'mario_database';
@@ -24,4 +24,4 @@ describe('The "sounds" table', () => {
       await client.end();
     }
   });
-});
+});*/
