40edeb09b34ea43c0a80d3bed80b9d0f6e0152b2
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 14:20:45 -0500

    1560.1

---
+++ freeCodeCamp/test/1560.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Client } = require('pg');
+
+const database = 'mario_database';
+const connectionString = `postgresql://postgres@localhost:5432/${database}`;
+const client = new Client({
+  connectionString: connectionString,
+});
+
+describe('The "character_actions" table', () => {
+  it('should have three rows for Daisy\'s actions', async () => {
+    const query = `SELECT * FROM character_actions WHERE character_id = 6 AND action_id = 1 OR character_id = 6 AND action_id = 2 OR character_id = 6 AND action_id = 3;`;
+
+    try {
+      await client.connect();
+      const res = await client.query(query);
+
+  	  assert(res.rows.length >= 3);
+    } catch (err) {
+      assert(false);
+    } finally {
+      await client.end();
+    }
+  });
+});
