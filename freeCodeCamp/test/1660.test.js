33d64af35a4ca2846ab4918f02f04de81d069fd4
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 14:53:23 -0500

    1660.1

---
+++ freeCodeCamp/test/1660.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { getLastQueryResult, getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog, lastQueryResult;
+  before(async () => {
+		lastLog = await getLastLog(true);
+		lastQueryResult = await getLastQueryResult();
+  });
+
+  it('view all the data from "characters" and "more_info" with a JOIN statement', () => {
+		const test1 = /fulljoin/i.test(lastLog);
+		const test2 = /characters\.character_id=more_info\.character_id/i.test(lastLog) || /more_info\.character_id=characters\.character_id/i.test(lastLog);
+		const correctNumberOfRows = /\(8$/.test(lastQueryResult);
+
+    assert(correctNumberOfRows && test1 && test2);
+  });
+});
