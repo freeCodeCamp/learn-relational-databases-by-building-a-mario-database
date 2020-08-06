e795c1e5169d62fa0f04995f50230ca3fad24f49
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 14:58:58 -0500

    1680.1

--- freeCodeCamp/test/1680.test.js
+++ freeCodeCamp/test/1680.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastQueryResult, getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -10,9 +10,10 @@ describe('You', () => {
 
   it('view all the data from "characters" and "more_info" with a JOIN statement', () => {
 		const test1 = /fulljoin/i.test(lastLog);
-		const test2 = /characters\.character_id=sounds\.character_id/i.test(lastLog) || /sounds\.character_id=characters\.character_id/i.test(lastLog);
-		const correctNumberOfRows = /\(12rows\)/.test(lastQueryResult);
+		const test2 = /character_actions\.character_id=characters\.character_id/i.test(lastLog);
+		const test3 = /character_actions\.action_id=actions\.action_id/i.test(lastLog);
+		const correctNumberOfRows = /\(21rows\)/.test(lastQueryResult);
 
-    assert(correctNumberOfRows && test1 && test2);
+    assert(correctNumberOfRows && test1 && test2 && test3);
   });
-});*/
+});
