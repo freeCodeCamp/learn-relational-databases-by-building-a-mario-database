a5e8f444ae9063b5fd34ef5f92f3ab52c8e1fccb
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 14:54:56 -0500

    1670.1

--- .freeCodeCamp/test/1670.test.js
+++ .freeCodeCamp/test/1670.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastQueryResult, getLastLog } = require('./utils');
 
 describe('You', () => {
@@ -7,9 +7,9 @@ describe('You', () => {
     const lastQueryResult = await getLastQueryResult();
     const test1 = lastLog.match(/fulljoin/gi).length === 1;
     const column_1 = /character_id/.test(lastQueryResult);
-    const column_2 = /more_info_id/.test(lastQueryResult);
-    const correctNumberOfRows = /\(7rows\)/.test(lastQueryResult);
+    const column_2 = /sound_id/.test(lastQueryResult);
+    const correctNumberOfRows = /\(12rows\)/.test(lastQueryResult);
 
     assert(correctNumberOfRows && test1 && column_1 && column_2);
   });
-});*/
+});
