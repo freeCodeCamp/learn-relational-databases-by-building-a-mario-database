4d81190af18aca02fae70b33df1a84230461baa1
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 14:19:49 -0500

    1550.1

---
+++ freeCodeCamp/test/1550.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastLog } = require('./utils');
+
+describe('You', () => {
+  let lastLog;
+  before(async () => {
+    lastLog = await getLastLog(true);
+  });
+
+  it('should view all the data in "actions"', () => {
+    const selectAll = /statement:select\*fromcharacter_actions;/;
+    assert(selectAll.test(lastLog));
+  });
+});
