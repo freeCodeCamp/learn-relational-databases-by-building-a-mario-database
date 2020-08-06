c4273ae3390904cf60b40823ab9bf92f7295fe77
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-06 11:56:26 -0500

    1340.1

--- .freeCodeCamp/db.sql
+++ .freeCodeCamp/db.sql
@@ -208,6 +208,7 @@ INSERT INTO public.more_info VALUES (7, '1990-04-13', 162, 59.1, 7);
 
 INSERT INTO public.sounds VALUES (1, 'its-a-me.wav', 1);
 INSERT INTO public.sounds VALUES (2, 'yippee.wav', 1);
+INSERT INTO public.sounds VALUES (3, 'ha-ha.wav', 2);
 
 
 --
@@ -228,7 +229,7 @@ SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 7, true);
 -- Name: sounds_sound_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.sounds_sound_id_seq', 2, true);
+SELECT pg_catalog.setval('public.sounds_sound_id_seq', 3, true);
 
 
 --
